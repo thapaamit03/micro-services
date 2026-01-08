26c9c3d6a62273a2d0f65715f3b7e4852b6ac148
thapaamit03 <amitthapamagar8@gmail.com>
2026-01-08 20:27:25 +0545

    Implement media service with database connection, request logging, and rate limiting

---
+++ Media-service/src/server.js
@@ -0,0 +1,52 @@
+const express=require('express');
+const cors=require('cors');
+const helmet=require('helmet');
+const mongoose=require('mongoose');
+const logger = require('./utils/logger');
+const errorHandler=require('./middleware/errorHandler');
+const mediaRoutes=require('./routes/mediaRoutes')
+const {rateLimit}=require('express-rate-limit');
+const app=express();
+const PORT=process.env.PORT;
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+mongoose.connect(process.env.MONGOODB_URL)
+.then(()=>logger.info("Database connected successfully"))
+.catch((e)=>logger.error("error while connecting to db",e.message))
+
+
+
+app.use((req,res,next)=>{
+
+    logger.info(`Request url :${req.url} and method:${req.method}`);
+    next();
+})
+
+const sensitiveEndPointLimiter=rateLimit({
+    windowMs:1024*1024*15,
+    max:20,
+    standardHeaders:true,
+    legacyHeaders:false,
+    handler:(req,res)=>{
+        logger.warn(`sensitive endpoint rate limit exceed for id:${req.ip}`)
+        res.status(429).json({
+            message:"too many request",
+            success:false
+        })
+    }
+}
+)
+app.use('/api/media',mediaRoutes);
+
+app.use(errorHandler);
+app.listen(PORT,()=>{
+
+    logger.info(`Media service is running in port:${PORT}`)
+}
+)
+
+process.on("unhandledRejection",(reason,promise)=>{
+    logger.info( 'unhandled rejection at',promise,'reason',reason)
+})  
